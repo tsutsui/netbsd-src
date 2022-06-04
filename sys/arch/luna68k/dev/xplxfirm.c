feb7258e53fca2f002478b3c92a91bc36cc0229b
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2022-06-05 06:48:49 +0900

    xplxfirm.c: KNF and const'ify.

--- sys/arch/luna68k/dev/xplxfirm.c
+++ sys/arch/luna68k/dev/xplxfirm.c
@@ -2,17 +2,14 @@
  * XXX TODO:
  * - add copyright notice (moveccr?)
  * - consider how xplx.inc should be handled
- * - make builtin_xplx[] and xplx const?
- * - ssize_t or size_t?
- * - KNF
  */
 #if !defined(_KERNEL)
 #include <stdint.h>
 #include <unistd.h>
 #endif
 
-uint8_t builtin_xplx[] = {
+const uint8_t builtin_xplx[] = {
 #include "xplx/xplx.inc"
 };
-ssize_t xplx_size = sizeof(builtin_xplx);
-uint8_t *xplx = builtin_xplx;
+const size_t xplx_size = sizeof(builtin_xplx);
+const uint8_t *xplx = builtin_xplx;
