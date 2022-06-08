1e346fdbd8223ce140745718035fc98a3ba33adc
isaki68k <isaki@pastel-flower.jp>
2022-06-08 19:30:52 +0900

    psgpam: スタイル修正。外部宣言に extern をつける風習はないようだ。

--- sys/arch/luna68k/dev/psgpam_enc.h
+++ sys/arch/luna68k/dev/psgpam_enc.h
@@ -40,22 +40,22 @@ struct psgpam_codecvar {
 	u_int sample_rate;
 };
 
-extern void psgpam_init_context(struct psgpam_codecvar *, u_int);
+void psgpam_init_context(struct psgpam_codecvar *, u_int);
 
-extern void psgpam_aint_to_pam2a(audio_filter_arg_t *);
-extern void psgpam_aint_to_pam2b(audio_filter_arg_t *);
-extern void psgpam_aint_to_pam3a(audio_filter_arg_t *);
-extern void psgpam_aint_to_pam3b(audio_filter_arg_t *);
-extern void psgpam_aint_to_pcm1(audio_filter_arg_t *);
-extern void psgpam_aint_to_pcm2(audio_filter_arg_t *);
-extern void psgpam_aint_to_pcm3(audio_filter_arg_t *);
+void psgpam_aint_to_pam2a(audio_filter_arg_t *);
+void psgpam_aint_to_pam2b(audio_filter_arg_t *);
+void psgpam_aint_to_pam3a(audio_filter_arg_t *);
+void psgpam_aint_to_pam3b(audio_filter_arg_t *);
+void psgpam_aint_to_pcm1(audio_filter_arg_t *);
+void psgpam_aint_to_pcm2(audio_filter_arg_t *);
+void psgpam_aint_to_pcm3(audio_filter_arg_t *);
 
-extern void psgpam_aint_to_pam2a_d(audio_filter_arg_t *);
-extern void psgpam_aint_to_pam2b_d(audio_filter_arg_t *);
-extern void psgpam_aint_to_pam3a_d(audio_filter_arg_t *);
-extern void psgpam_aint_to_pam3b_d(audio_filter_arg_t *);
-extern void psgpam_aint_to_pcm1_d(audio_filter_arg_t *);
-extern void psgpam_aint_to_pcm2_d(audio_filter_arg_t *);
-extern void psgpam_aint_to_pcm3_d(audio_filter_arg_t *);
+void psgpam_aint_to_pam2a_d(audio_filter_arg_t *);
+void psgpam_aint_to_pam2b_d(audio_filter_arg_t *);
+void psgpam_aint_to_pam3a_d(audio_filter_arg_t *);
+void psgpam_aint_to_pam3b_d(audio_filter_arg_t *);
+void psgpam_aint_to_pcm1_d(audio_filter_arg_t *);
+void psgpam_aint_to_pcm2_d(audio_filter_arg_t *);
+void psgpam_aint_to_pcm3_d(audio_filter_arg_t *);
 
 #endif /* !_PSGPAM_ENC_H_ */
