54e4f8d48a8e0220a5bdd949daab0b107ab34ef0
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2022-07-28 03:24:21 +0900

    m88k: pull vunmapbuf() fix from OpenBSD vm_machdep.c rev 1.15.
    > In vunmapbuf(), explicitely remove mappings before invoking uvm_km_free().
    > Even if the latter would end up removing the mappings by itself, it would
    > do so using pmap_remove() because phys_map is not intrsafe; but some
    > platforms use pmap_kenter_pa() in vmapbuf(). By removing the mappings
    > ourselves, we can ensure the remove function used matches the enter function
    > which has been used.
    > Discussed and theoretical ok art@
    
    This fixes KASSERT(pg->uobject == NULL && pg->uanon == NULL) in uvm_km.c
    and failures on raw device accesses.

--- sys/arch/m88k/m88k/vm_machdep.c
+++ sys/arch/m88k/m88k/vm_machdep.c
@@ -323,6 +323,8 @@ vunmapbuf(bp, len)
 	addr = trunc_page((vaddr_t)bp->b_data);
 	off = (vaddr_t)bp->b_data & PGOFSET;
 	len = round_page(off + len);
+	pmap_remove(vm_map_pmap(phys_map), addr, addr + len);
+	pmap_update(vm_map_pmap(phys_map));
 	uvm_km_free_wakeup(phys_map, addr, len);
 	bp->b_data = bp->b_saveaddr;
 	bp->b_saveaddr = 0;
