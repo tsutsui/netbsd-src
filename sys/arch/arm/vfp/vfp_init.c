fac851b226d1d51c8e4503601a961f1b74b6a1e3
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2026-03-20 06:09:38 +0900

    arm/vfp: add and recoginize Cortex-A72 VFP.
    With this change NetBSD/evbarm-earmv7hf kernel works upto multi-user
    on Raspberry Pi 4B.

--- sys/arch/arm/vfp/vfp_init.c
+++ sys/arch/arm/vfp/vfp_init.c
@@ -115,6 +115,7 @@ load_vfpregs(const struct vfpreg *fregs)
 	case FPU_VFP_CORTEXA15_QEMU:
 	case FPU_VFP_CORTEXA53:
 	case FPU_VFP_CORTEXA57:
+	case FPU_VFP_CORTEXA72:
 #endif
 		load_vfpregs_hi(fregs->vfp_regs);
 #ifdef CPU_ARM11
@@ -139,6 +140,7 @@ save_vfpregs(struct vfpreg *fregs)
 	case FPU_VFP_CORTEXA15_QEMU:
 	case FPU_VFP_CORTEXA53:
 	case FPU_VFP_CORTEXA57:
+	case FPU_VFP_CORTEXA72:
 #endif
 		save_vfpregs_hi(fregs->vfp_regs);
 #ifdef CPU_ARM11
@@ -322,6 +324,7 @@ vfp_attach(struct cpu_info *ci)
 	case FPU_VFP_CORTEXA17:
 	case FPU_VFP_CORTEXA53:
 	case FPU_VFP_CORTEXA57:
+	case FPU_VFP_CORTEXA72:
 		if (armreg_cpacr_read() & CPACR_V7_ASEDIS) {
 			model = "VFP 4.0+";
 		} else {
