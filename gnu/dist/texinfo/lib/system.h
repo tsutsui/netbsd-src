77612f2e7982d7276c515e1bd39e4d2688922979
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2026-09-23 17:30:50 +0900

    texinfo: fix tools build with gcc 15
    Pulled from external/gpl2/texinfo/dist/lib/system.h rev 1.3.

--- gnu/dist/texinfo/lib/system.h
+++ gnu/dist/texinfo/lib/system.h
@@ -119,6 +119,13 @@ extern char *strerror ();
 #define PATH_MAX _POSIX_PATH_MAX
 #endif
 
+/*
+ * configure never checks for the declarations of strcasecmp and strncasecmp.
+ * Hence the HAVE_DECL_*s for them are never defined in config.
+ * GCC 15.1 make conflicting prototypes an error.
+ * Therefore disable the following two conditions to appease GCC 15.1.
+ */
+#if 0 /* GCC 15.1 conflicting prototypes */
 #ifndef HAVE_DECL_STRCASECMP
 extern int strcasecmp ();
 #endif
@@ -126,6 +133,7 @@ extern int strcasecmp ();
 #ifndef HAVE_DECL_STRNCASECMP
 extern int strncasecmp ();
 #endif
+#endif /* GCC 15.1 conflicting prototypes */
 
 #ifndef HAVE_DECL_STRCOLL
 extern int strcoll ();
